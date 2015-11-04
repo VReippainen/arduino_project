daeab6b171b26c664411f2eaa34d285319e025d9
Ville Reippainen <ville.reippainen@gmail.com>
2015-11-04 16:11:05 +0200

    Fixed bugs and created SetMotors() function

--- new_arduino_project/new_arduino_project.ino
+++ new_arduino_project/new_arduino_project.ino
@@ -11,30 +11,34 @@
 #define BT_RX 9
 #define BT_Baudrate 9600
 #define BT_DEBUG
-#define MPU6050_DEBUG
+//#define MPU6050_DEBUG
+
+
+//Motor variables
+#define LEFT_MOTOR_PIN 5
+#define RIGHT_MOTOR_PIN 6
+int left_motor_speed = 0;
+int right_motor_speed = 0;
 
 SoftwareSerial BT(BT_TX, BT_RX);
-char speed; //0 means backwards, 2 is front, 1 means stop
-char dir; //0 is left, 2 is right. 1 means no direction.
-char UP = '2';
-char DOWN = '0';
-char UPLEFT = 'B';
-char UPRIGHT = 'C';
-char DOWNLEFT = 'E';
-char DOWNRIGHT = 'F';
-char RESET = '6';
+char speed1 = 1; //0 means backwards, 2 is front, 1 means stop
+char dir1 = 1; //0 is left, 2 is right. 1 means no direction.
+const char UP = 'w';
+const char DOWN = 's';
+const char LEFT = 'a';
+const char RIGHT = 'd';
 char led;
 // Loop variables
-int LOOPTIME = 10; //In milliseconds
-int loop_start_time;
-int loop_used_time;
+const int LOOPTIME = 15; //In milliseconds
+long loop_start_time;
+long loop_used_time;
 //MPU variables, copied from
 MPU6050 mpu(0x69); // AD0 low = 0x68
 bool dmpReady = false; // set true if DMP init was successful
 uint8_t mpuIntStatus; // holds actual interrupt status byte from MPU
 uint8_t devStatus; // return status after each device operation (0 = success, !0 = error)
 uint16_t packetSize; // expected DMP packet size (default is 42 bytes)
-uint16_t fifoCount; // count of all bytes currently in FIFO
+int16_t fifoCount; // count of all bytes currently in FIFO
 uint8_t fifoBuffer[64]; // FIFO storage buffer
 
 //MPU orientation
@@ -52,8 +56,10 @@ void dmpDataReady() {
 }
 
 void setup() {
+    analogWrite(RIGHT_MOTOR_PIN, 0);
+    analogWrite(LEFT_MOTOR_PIN, 0);
     led = 0;
-    digitalWrite(RESET_PIN, HIGH);
+    //digitalWrite(RESET_PIN, HIGH);
     digitalWrite(LED, LOW);
     delay(20);
     pinMode(RESET_PIN, OUTPUT);
@@ -63,7 +69,7 @@ void setup() {
     Wire.begin(); //join I2C bus
     TWBR = 24;
 
-    Serial.begin(115200);
+    Serial.begin(57600);
     while (!Serial); // wait for Leonardo enumeration, others continue immediately
 
     // initialize device
@@ -103,7 +109,8 @@ void setup() {
 
         // get expected DMP packet size for later comparison
         packetSize = mpu.dmpGetFIFOPacketSize();
-    } else {
+        //Serial.println(packetSize);
+    }else {
         // ERROR!
         // 1 = initial memory load failed
         // 2 = DMP configuration updates failed
@@ -116,52 +123,59 @@ void setup() {
 }
 
 void read_bluetooth() {
-    char val;
+    char val = '0';
     if (BT.available()) {
         val = BT.read();
     }
-    if (val == UP) {
-        speed = 2;
-        dir = 1;
-    }
-    if (val == DOWN) {
-        speed = 0;
-        dir = 1;
-    }
-    if (val == UPLEFT) {
-        speed = 2;
-        dir = 0;
-    }
-    if (val == UPRIGHT) {
-        speed = 2;
-        dir = 2;
-    }
-    if (val == DOWNLEFT) {
-        speed = 0;
-        dir = 0;
-    }
-    if (val == DOWNRIGHT) {
-        speed = 0;
-        dir = 2;
-    if (val == RESET) {
-        Serial.println("Reset!");
-        pinMode(RESET_PIN, LOW);
-    }
-    } else {
-        speed = 1;
-        dir = 1;
-    }
+    if(isalpha(val) && val != 'b'){
+      
+      Serial.println("Is alpha!");
+      if (val == UP) {
+         // Serial.println("UP");
+          speed1 = 2;
+          dir1 = 1;
+      }
+      
+      else if (val == LEFT) {
+          //Serial.println("LEFT");
+          speed1 = 1;
+          dir1 = 0;
+      }
+      else if (val == RIGHT) {
+          //Serial.println("RIGHT");
+          speed1 = 1;
+          dir1 = 2;
+      }
+      else if (val == DOWN) {
+          //Serial.println("DOWN");
+          speed1 = 0;
+          dir1 = 1;
+      }
+          
+          
+
+      else {
+          speed1 = 1;
+          dir1 = 1;
+      }
+      #ifdef BT_DEBUG
+          Serial.print("Val: ");
+          Serial.print(val);
+          Serial.print("\tSpeed: ");
+          Serial.print(speed1, DEC);
+          Serial.print("\tDir: ");
+          Serial.println(dir1, DEC);
+      #endif
+    } 
 }
 
 void read_gyro() {
     if (!dmpReady) return;
     while (!mpuInterrupt && fifoCount < packetSize) {
     }
-
     // reset interrupt flag and get INT_STATUS byte
     mpuInterrupt = false;
     mpuIntStatus = mpu.getIntStatus();
-
     // get current FIFO count
     fifoCount = mpu.getFIFOCount();
 
@@ -170,19 +184,15 @@ void read_gyro() {
         // reset so we can continue cleanly
         mpu.resetFIFO();
         Serial.println(F("FIFO overflow!"));
-
-        // otherwise, check for DMP data ready interrupt (this should happen frequently)
-    } else if (mpuIntStatus & 0x02) {
+    }
+        // otherwise, check for DMP data ready interrupt (this should happen frequently)}
+     else if (mpuIntStatus & 0x02) {
         // wait for correct available data length, should be a VERY short wait
         while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
-
         // read a packet from FIFO
         mpu.getFIFOBytes(fifoBuffer, packetSize);
-
         // track FIFO count here in case there is > 1 packet available
         // (this lets us immediately read more without waiting for an interrupt)
-        fifoCount -= packetSize;
-
         // display Euler angles in degrees
         mpu.dmpGetQuaternion(&q, fifoBuffer);
         mpu.dmpGetGravity(&gravity, &q);
@@ -195,22 +205,41 @@ void read_gyro() {
         Serial.print("\t");
         Serial.println(ypr[2] * 180 / M_PI);
 #endif
+        mpu.resetFIFO();
     }
 }
-void set_led(){
-    if(led) digitalWrite(LED, HIGH);
-    else digitalWrite(LED,LOW);
+
+void set_led() {
+    if (led) digitalWrite(LED, HIGH);
+    else digitalWrite(LED, LOW);
     led = !led;
 }
 
-    void loop() {
-        read_bluetooth();
-        //read_gyro();
-        set_led();
-        loop_used_time = millis()-loop_start_time;
-        if(loop_used_time < LOOPTIME){
-            delay(LOOPTIME-loop_used_time);
-        }
-        loop_start_time = millis();
-        
+void doCalculations() {
+}
+
+void setMotors() {
+    
+    //Maps variables left_motor_speed and right_motor speed to correct
+    //range and writes them through PWM pins to motor controllers. Positive values
+    //means the positive direction and opposite.
+    int write_to_left = map(left_motor_speed,-127,128,0,255);
+    int write_to_right = map(right_motor_speed, -127, 128, 0, 255);
+    analogWrite(LEFT_MOTOR_PIN, write_to_left);
+    analogWrite(RIGHT_MOTOR_PIN, write_to_right);
+}
+
+void loop() {
+    loop_start_time = millis();
+    read_bluetooth();
+    read_gyro();    
+    //doCalculations();
+    setMotors();
+    set_led();
+    loop_used_time = millis() - loop_start_time;
+    if (loop_used_time < LOOPTIME) {
+        delay(LOOPTIME - loop_used_time);
     }
+    
+
+}
